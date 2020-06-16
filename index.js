99125fa348641581e3add423be8ab3b978c56945
GaryGSC <GaryGSC@users.noreply.github.com>
2020-06-16 10:21:19 -0600

    Change link to RFC based on credentials
    Fixes: #2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { getInput, setOutput, setFailed, debug } = require('@actions/core')
 const github = require('@actions/github')
 const wso2 = require('byu-wso2-request')
+const jsonWebToken = require('jsonwebtoken')
 
 async function run () {
   // Grab some inputs from GitHub Actions
@@ -38,7 +39,7 @@ async function run () {
     const bodyWithNetId = await requestWithRetry(optionsToGetNetId)
     const netId = bodyWithNetId.result[0].user_name
 
-    // Start the RFC
+    // Start the RFC (and figure out if we're doing it in sandbox or production)
     const optionsToStartRfc = {
       method: 'PUT',
       uri: 'https://api.byu.edu:443/domains/servicenow/standardchange/v1/change_request',
@@ -55,11 +56,20 @@ async function run () {
         ]
       }
     }
-    const bodyWithResultsOfStartingRfc = await requestWithRetry(optionsToStartRfc)
+    let errorOccurredWhileGettingCredentialsType = false
+    const [bodyWithResultsOfStartingRfc, credentialsType] = await Promise.all([
+      requestWithRetry(optionsToStartRfc),
+      getTypeOfCredentials().catch(() => { errorOccurredWhileGettingCredentialsType = true; return 'PRODUCTION' })
+    ])
+    if (errorOccurredWhileGettingCredentialsType) {
+      console.log('⚠️ An error occurred while trying to determine if production or sandbox credentials were used for ServiceNow. ⚠️')
+      console.log('The standard change was still started in the correct environment.')
+      console.log('So the link provided below will be for the production environment, even though you may have used sandbox credentials. 🤷')
+    }
     const result = bodyWithResultsOfStartingRfc.result[0]
 
     console.log(`RFC Number: ${result.number}`)
-    console.log(`Link to RFC: https://it.byu.edu/change_request.do?sysparm_query=number=${result.number}`)
+    console.log(`Link to RFC: https://${credentialsType === 'PRODUCTION' ? 'it' : 'ittest'}.byu.edu/change_request.do?sysparm_query=number=${result.number}`)
 
     // Set outputs for GitHub Actions
     setOutput('change-sys-id', result.change_sys_id)
@@ -74,6 +84,13 @@ async function run () {
 
 function requestWithRetry (options) {
   return wso2.request(options).catch(() => wso2.request(options))
+}
+
+async function getTypeOfCredentials () {
+  const options = { uri: 'https://api.byu.edu:443/echo/v1/echo/test', simple: true }
+  const { Headers: { 'X-Jwt-Assertion': [jwt] } } = await requestWithRetry(options)
+  const decoded = jsonWebToken.decode(jwt)
+  return decoded['http://wso2.org/claims/keytype'] // 'PRODUCTION' | 'SANDBOX'
 }
 
 run()
